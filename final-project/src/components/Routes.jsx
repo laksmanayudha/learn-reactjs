f50ec2f2989af8bf17ec5584c8fcf2d26dc3f806
laksmanayudha <laksmanayudha22@gmail.com>
2022-06-22 20:52:23 +0800

    final project init

---
+++ final-project/src/components/Routes.jsx
@@ -0,0 +1,14 @@
+import {
+    BrowserRouter as Router,
+    Link,
+    Switch,
+    Route
+} from "react-router-dom";
+
+const Routes = () => {
+    return(
+        <h1>hello</h1>
+    )
+}
+
+export default Routes;
