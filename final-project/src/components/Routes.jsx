13dd68ef49907fa8d1577e34d338da72c6d8536b
laksmanayudha <laksmanayudha22@gmail.com>
2022-06-22 21:10:45 +0800

    final project init update

--- final-project/src/components/Routes.jsx
+++ final-project/src/components/Routes.jsx
@@ -7,7 +7,24 @@ import {
 
 const Routes = () => {
     return(
-        <h1>hello</h1>
+        <Router>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+            
+            <Switch>
+                <Route exact path="/">
+                    <h1>hello</h1>
+                </Route>
+                <Route path="/login">
+                    <h1>Login</h1>
+                </Route>
+                <Route>
+                    <h1>hehe</h1>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
